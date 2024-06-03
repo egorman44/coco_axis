8e31876f7c18d0b55fa5e0f6500af105f6e44635
egorman44 <easmirnov44@gmail.com>
2024-06-03 21:37:33 +0400

    [AXIS] Add send_interleaved_pkts() method. Fix hang for monitor. Substitute await RisingEdge(tvalid) with while loop.

--- axis.py
+++ axis.py
@@ -69,18 +69,20 @@ class AxisDriver:
         if(self.axis_if.tlast is not None):
             if(last_word):
                 self.axis_if.tlast.value = 1
+            else:
+                self.axis_if.tlast.value = 0
 
-    def drive_tuser(self, last_word):
+    def drive_tuser(self, pkt, last_word):
         if(self.axis_if.tuser is not None):
             if(last_word):
-                self.axis_if.tuser.value = self.pkt.user[0]
+                self.axis_if.tuser.value = pkt.user[0]
             else:
-                self.axis_if.tuser.value = 0
+                self.axis_if.tuser.value = pkt.user[0]
 
-    def drive_tkeep(self, last_word):
+    def drive_tkeep(self, pkt, last_word):
         if self.axis_if.tkeep is not None:
-            if last_word and self.pkt.pkt_size % self.width != 0:
-                tkeep_msb_pos = self.pkt.pkt_size % self.width
+            if last_word and pkt.pkt_size % self.width != 0:
+                tkeep_msb_pos = pkt.pkt_size % self.width
             else:
                 tkeep_msb_pos = self.width
             if self.axis_if.tkeep_type == 'ffs':
@@ -91,14 +93,14 @@ class AxisDriver:
                 tkeep = int(f"{tkeep:0{self.width}b}"[::-1],2)
             self.axis_if.tkeep.value = tkeep
 
-    def drive_tdata(self, last_word, word_num):
+    def drive_tdata(self, pkt, last_word, word_num):
         # TODO: check that Endianess is matched for all combinations
         # Prepare data. Put it into the list
         data_list = [0] * self.width
         if(last_word):
-            data_list = self.pkt.data[self.width*word_num:]
+            data_list = pkt.data[self.width*word_num:]
         else:            
-            data_list = self.pkt.data[self.width*word_num:self.width*(word_num+1)]
+            data_list = pkt.data[self.width*word_num:self.width*(word_num+1)]
         # append if last cycle and data < self.width
         if(len(data_list) < self.width):
             data_list = data_list + [0]*(self.width-len(data_list))
@@ -123,7 +125,6 @@ class AxisDriver:
             if self.pkt0_word0 == 1:
                 data_list.reverse()
         # Write data into IF in depends on the interface type
-        #print(f"data_list = {data_list}")
         if(self.unpack == 'unpacked'):
             self.axis_if.tdata.value = data_list
         elif(self.unpack == 'packed'):
@@ -141,49 +142,49 @@ class AxisDriver:
         if self.axis_if.tvalid is not None:
             if(self.flow_ctrl == 'flow_en'):
                 self.flow_ctrl = random.choice(['one_valid_one_nonvalid', 'one_valid_some_nonvalid', 'some_valid_some_nonvalid'])
-            
-            if(self.flow_ctrl ==  'one_valid_one_nonvalid'):                
-                if self.tvalid_state:
-                    if tnx_completed:
-                        self.tvalid_state = 0                            
-                else:
-                    self.tvalid_state = 1
-                self.axis_if.tvalid.value = self.tvalid_state
-            elif(self.flow_ctrl == 'one_valid_some_nonvalid'):
-                if self.tvalid_state:
-                    tvalid_val = 1                        
-                    self.tvalid_delay = random.randint(1,5)
-                    self.tvalid_state = 0
-                else:
-                    tvalid_val = 0
+
+            time_ns = get_sim_time(units='ns')
+            if tnx_completed == 0:
+                self.axis_if.tvalid.value = 1
+            else:
+                if(self.flow_ctrl ==  'one_valid_one_nonvalid'):                
+                    if self.tvalid_state:
+                        if tnx_completed:
+                            self.tvalid_state = 0                            
+                    else:
+                        self.tvalid_state = 1
+                    self.axis_if.tvalid.value = self.tvalid_state
+                elif(self.flow_ctrl == 'one_valid_some_nonvalid'):
+                    if self.tvalid_state:
+                        tvalid_val = 1                        
+                        self.tvalid_delay = random.randint(1,5)
+                        self.tvalid_state = 0
+                    else:
+                        tvalid_val = 0
+                        if self.tvalid_delay:
+                            self.tvalid_delay -= 1
+                        else:
+                            self.tvalid_state = 1
+                    self.axis_if.tvalid.value = tvalid_val
+                elif(self.flow_ctrl == 'some_valid_some_nonvalid'):
                     if self.tvalid_delay:
                         self.tvalid_delay -= 1
                     else:
-                        self.tvalid_state = 1
-                self.axis_if.tvalid.value = tvalid_val
-            elif(self.flow_ctrl == 'some_valid_some_nonvalid'):
-                if self.tvalid_delay:
-                    self.tvalid_delay -= 1
+                        self.tvalid_delay = random.randint(1,5)
+                        self.tvalid_state = self.tvalid_state ^ 1
+                    self.axis_if.tvalid.value = self.tvalid_state
+    
                 else:
-                    self.tvalid_delay = random.randint(1,5)
-                    self.tvalid_state = self.tvalid_state ^ 1
-                self.axis_if.tvalid.value = self.tvalid_state
-
-            else:
-                self.axis_if.tvalid.value = 1
+                    self.axis_if.tvalid.value = 1
         
 
     '''
     send_pkt() is a main corouting that controls the packet transmition
-
     '''
     
     async def send_pkt(self, pkt):
-        self.pkt = pkt
         self.tvalid_state = 1
         self.tvalid_delay = random.randint(1,5)
-        word_list = pkt.get_word_list(self.width)
-        tvalid_val = 1
         tnx_completed = 0
         for x in range(pkt.delay):
             await RisingEdge(self.axis_if.aclk)
@@ -199,9 +200,9 @@ class AxisDriver:
             Send stimulus to DUT
             '''
             self.drive_tlast(last_word)
-            self.drive_tuser(last_word)
-            self.drive_tkeep(last_word)
-            self.drive_tdata(last_word, word_num)
+            self.drive_tuser(pkt, last_word)
+            self.drive_tkeep(pkt, last_word)
+            self.drive_tdata(pkt, last_word, word_num)
             self.drive_tvalid(tnx_completed)
             await RisingEdge(self.axis_if.aclk)
             tnx_completed = self.check_transaction_completion()
@@ -213,6 +214,58 @@ class AxisDriver:
         if self.axis_if.tlast is not None:
             self.axis_if.tlast.value = 0
         #tvalid_state = 1
+
+    '''
+    send_interleaved_pkts() takes a list of packets and interleave it
+    '''
+    
+    async def send_interleaved_pkts(self, pkts):
+        self.tvalid_state = 1
+        self.tvalid_delay = random.randint(1,5)
+        self.current_index = 0
+        pointers = [0] * len(pkts)
+        packets_len = [math.ceil(len(x.data)/self.width) for x in pkts]
+        tnx_completed = 0
+        for x in range(pkts[0].delay):
+            await RisingEdge(self.axis_if.aclk)
+        while self.get_pkt_indx(packets_len, pointers, tnx_completed):
+            if(pointers[self.current_index] == packets_len[self.current_index]-1):
+                last_word = 1
+            else:
+                last_word = 0
+            self.drive_tlast(last_word)
+            self.drive_tuser(pkts[self.current_index], last_word)
+            self.drive_tkeep(pkts[self.current_index], last_word)
+            self.drive_tdata(pkts[self.current_index], last_word, pointers[self.current_index])
+            self.drive_tvalid(tnx_completed)
+            await RisingEdge(self.axis_if.aclk)
+            tnx_completed = self.check_transaction_completion()
+            if(tnx_completed):
+                pointers[self.current_index] += 1
+            
+        if self.axis_if.tvalid is not None:
+            self.axis_if.tvalid.value = 0
+        if self.axis_if.tlast is not None:
+            self.axis_if.tlast.value = 0
+
+    '''
+    Evaluates index of the packet that needs to be send in send_interleaved_pkts()
+    '''
+    
+    def get_pkt_indx(self, pkts_len, pointers, tnx_completed):
+        if tnx_completed:
+            iter_cntr = 0
+            while iter_cntr < len(pkts_len):
+                self.current_index = (self.current_index + 1) % len(pkts_len)
+                iter_cntr += 1
+                if pointers[self.current_index] != pkts_len[self.current_index]:
+                    return True
+            return False
+        else:
+            return True
+
+            #raise ValueError(f"All packets were transmitted.")
+            
         
 '''
 AxisMonitor
@@ -248,16 +301,15 @@ class AxisMonitor:
                     tkeep_int_val |= self.axis_if.tkeep[i].value << i                
             elif(self.axis_if.tkeep_type == 'ffs'):
                 if(self.axis_if.tkeep.value == 0):
-                    #assert False, f"[AXIS_ERROR] TKEEP is 0."
                     tkeep_int_val = 0
                 else:
                     tkeep_int_val = (self.axis_if.tkeep.value << 1)-1
             tkeep_int = 0
             self.pkt_size += countones(tkeep_int_val)
             for byte_indx in range(0, self.width):
-                if check_pos(tkeep_int_val, byte_indx):
+                if check_pos(tkeep_int_val, byte_indx):                    
                     tkeep_int |= 0xFF << (8 * byte_indx)
-                tkeep_int = int(bin(tkeep_int)[:1:-1], 2)                        
+            tkeep_int = int(bin(tkeep_int)[:1:-1], 2)                        
         else:
             tkeep_int = (2 ** (8*self.width))-1
             self.pkt_size += self.width
@@ -287,7 +339,7 @@ class AxisMonitor:
                         indx += 1                        
                 elif(self.unpack == 'packed'):                    
                     tdata_int = self.axis_if.tdata.value.integer
-                    if(self.pkt0_word0):
+                    if(self.pkt0_word0 == 0):
                         tdata_rev = 0
                         if(self.width > 1):                            
                             for byte_indx in range(self.width):
@@ -366,12 +418,12 @@ class AxisResponder:
         elif(self.mode == 'BACKPRESSURE_1'):
             while True:
                 self.axis_if.tready.value = 1
-                if self.axis_if.tvalid.value == 0:
-                    await RisingEdge(self.axis_if.tvalid)
+                while self.axis_if.tvalid.value == 0:
+                    await RisingEdge(self.axis_if.aclk)
                 await RisingEdge(self.axis_if.aclk)
                 self.axis_if.tready.value = 1
                 interval = random.randint(1,5)
-                for cycle_num in range(0,interval):
+                for cycle_num in range(interval):
                     await RisingEdge(self.axis_if.aclk)
                 self.axis_if.tready.value = 0
                 interval = random.randint(1,5)
@@ -380,8 +432,9 @@ class AxisResponder:
         elif(self.mode == 'BACKPRESSURE_0'):
             while True:
                 self.axis_if.tready.value = 0
-                if self.axis_if.tvalid.value == 0:
-                    await RisingEdge(self.axis_if.tvalid)
+                while self.axis_if.tvalid.value == 0:
+                    await RisingEdge(self.axis_if.aclk)
+                await RisingEdge(self.axis_if.aclk)
                 interval = random.randint(1,5)
                 for cycle_num in range(0,interval):
                     await RisingEdge(self.axis_if.aclk)                    
